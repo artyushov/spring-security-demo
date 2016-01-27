43dfe88888109c0d93b6ce690856ac1432b9af1b
artyushov <nikita.artyushov@gmail.com>
2016-01-27 12:08:22 +0200

    updated demo

--- src/main/java/me/artyushov/blog/WebSecurityConfig.java
+++ src/main/java/me/artyushov/blog/WebSecurityConfig.java
@@ -3,6 +3,8 @@ package me.artyushov.blog;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -18,15 +20,26 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("regularUser").password("123").roles("USER")
-                .and()
-                .withUser("admin").password("123abc").roles("USER", "ADMIN");
+        JdbcDaoImpl userDetailsService = new JdbcDaoImpl();
+        userDetailsService.setJdbcTemplate(jdbcTemplate);
+        userDetailsService.setUsersByUsernameQuery(
+                "select name, password, 1 from User where name = ?");
+        userDetailsService.setAuthoritiesByUsernameQuery(
+                "select name, authority FROM UserAuthority where name = ?");
+
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+
+        auth.authenticationProvider(authenticationProvider);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
         http.authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/onlyForUsers").authenticated()
